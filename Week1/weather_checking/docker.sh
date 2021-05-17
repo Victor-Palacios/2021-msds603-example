e58d8a399815e4986e2cc35f712676c45ae16513
dianewoodbridge <dwoodbridge@usfca.edu>
2021-05-17 11:09:17 -0700

    added cron

--- Week1/weather_checking/docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker build -t weather-app .
-docker tag weather-app dwoodbridge/weather-app
-docker push dwoodbridge/weather-app
-#docker run -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID  -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY  -e API_KEY=$API_KEY -d -p 80:80 --name weather-app weather-app
