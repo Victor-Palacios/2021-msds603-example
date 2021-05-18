6bf90d40c33e33d39391a47672c8bad3c8dd22ca
dianewoodbridge <dwoodbridge@usfca.edu>
2021-05-18 14:37:45 -0700

    updated .py to pass pycodetest

--- Week1/weather_checking/application.py
+++ Week1/weather_checking/application.py
@@ -11,6 +11,7 @@ from user_definition import *
 
 application = Flask(__name__)
 
+
 def read_s3_obj(bucket_name, output_file):
     """ Read from s3 bucket"""
     try:
@@ -19,14 +20,11 @@ def read_s3_obj(bucket_name, output_file):
             aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY']
         )
         s3 = session.resource('s3')
-        print("connected")
         obj = s3.Object(bucket_name, output_file)
         body = obj.get()['Body'].read().decode('utf-8')
         return body
-    except:
-        print("not")
+    except KeyError:
         return ""
-
 
 
 @application.route('/', methods=['GET', 'POST'])
@@ -35,10 +33,6 @@ def index():
     """ index page -- shown on the beginning """
     body = read_s3_obj(bucket_name, output_file)
 
-    #s3 = boto3.resource('s3')
-    #obj = s3.Object(bucket_name, output_file)
-    #body = obj.get()['Body'].read().decode('utf-8')
-
     return render_template('index.html', output=body)
 
 
@@ -46,37 +40,39 @@ def index():
 def calculate():
     """ Read Google Distance API """
     api_key = os.environ['API_KEY']
-    url = f"http://api.openweathermap.org/data/2.5/weather?appid={api_key}&zip={zip},us&units=imperial"
+    url = f"http://api.openweathermap.org/data/2.5/weather?appid={api_key}" \
+          f"&zip={zip},us&units=imperial"
     response = requests.get(url)
     x = response.json()
-    print(url)
-    print(x)
     main = x['weather'][0]['main']
     temp = x['main']['temp']
 
     if (main in ['Clear', 'Clouds'] and 60 < temp < 85):
         msg = "Go for a walk"
     else:
-        msg ="Stay home"
+        msg = "Stay home"
     prev_reading = read_s3_obj(bucket_name, output_file)
     print(prev_reading)
 
     body = "{}\t{}\t{}\t{}\t{}\n".format(msg,
-                                           datetime.datetime.now(),
-                                           main,
-                                           temp,
-                                           prev_reading)
+                                         datetime.datetime.now(),
+                                         main,
+                                         temp,
+                                         prev_reading)
 
+    boto3.resource("s3")\
+         .Bucket(bucket_name)\
+         .put_object(Key=output_file, Body=body, ACL='public-read-write')
 
-    #s3 = boto3.resource("s3").Object(bucket_name,output_file).put(Body=body)
-    boto3.resource("s3").Bucket(bucket_name).put_object(Key=output_file, Body=body, ACL='public-read-write')
-
-    time.sleep(5) # Added this for working on EB - Needs a delay to update the access after putting an obj
+    # time.sleep(5)
+    # Added this for working on EB -
+    # Needs a delay to update the access after putting an obj
 
     return redirect("/index")
+
 
 if __name__ == '__main__':
     application.jinja_env.auto_reload = True
     application.config['TEMPLATES_AUTO_RELOAD'] = True
-    #application.debug = True
-    application.run(host ='0.0.0.0', port = 80, debug = True)
+    # application.debug = True
+    application.run(host='0.0.0.0', port=80, debug=True)
